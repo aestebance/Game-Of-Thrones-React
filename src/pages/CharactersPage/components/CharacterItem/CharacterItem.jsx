ba98899403f9b60f5c2439c48388738a59fd7365
Alejandro <aesteance@uoc.edu>
2020-12-08 00:32:03 +0100

    changes

---
+++ src/pages/CharactersPage/components/CharacterItem/CharacterItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './CharacterItem.scss';
+
+export default function CharacterItem(props) {
+
+    return (
+        <div className="div">
+            {/*<img className="c-characters__img img-fluid" src={props.character.image} alt={props.character.name}/>*/}
+        </div>
+    )
+}
--- src/pages/CharactersPage/components/CharactersGallery/CharactersGallery.jsx
+++ src/pages/CharactersPage/components/CharactersGallery/CharactersGallery.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import CharacterItem from "../CharacterItem/CharacterItem";
 import './CharactersGallery.scss';
-import SimpleBar from 'simplebar-react';
-import 'simplebar/dist/simplebar.min.css';
 
-export default function CharactersGallery (props) {
+export default function CharactersGallery(props) {
 
     return (
-        <div className="c-char-gallery">
-            <SimpleBar style={{maxHeight: 600 }}>
-            {props.characters.map((character, index) =>
-                <p key={index}>{character.name}</p>
-            )}
-            </SimpleBar>
+        <div className="c-char-gallery container-fluid">
+            <div className="row c-char-gallery__row">
+                    {props.characters.filter((char, i) => (i<12)).map((character, index) =>
+                        <div className="col-12 col-md-6 col-lg-4 col-xl-2 c-char-gallery__item" key={index}>
+                            <CharacterItem character={character}></CharacterItem>
+                        </div>
+                    )}
+            </div>
         </div>
     )
 }
