b4d7b0f37a3b4a076621d34376220ecd7da8e029
Alejandro <aesteance@uoc.edu>
2020-12-09 11:35:58 +0100

    changes

--- src/pages/CharactersPage/components/CharactersGallery/CharactersGallery.jsx
+++ src/pages/CharactersPage/components/CharactersGallery/CharactersGallery.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import CharacterItem from "../CharacterItem/CharacterItem";
 import './CharactersGallery.scss';
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
 
 export default function CharactersGallery(props) {
     return (
         <div className="c-char-gallery container-fluid">
-            <div className="row">
-                    {props.characters.filter((char, i) => (i < 12)).map((character, index) =>
+            <SimpleBar style={{height: "100%"}}>
+                <div className="row">
+                    {props.characters.filter((char, i) => (i < 24)).map((character, index) =>
                         <div className="char-container col-12 col-sm-6  col-md-4 col-lg-3 col-xl-2" key={index}>
                             <CharacterItem character={character}></CharacterItem>
                         </div>
                     )}
-            </div>
+                </div>
+            </SimpleBar>
         </div>
     )
 }
